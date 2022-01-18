7b618ae3fc17147b5cdf947c2413d8d7629374f6
plam <smallshell@gmail.com>
2022-01-17 16:56:58 -0800

    Added junit dependency to pom.xml

---
+++ src/test/java/guru/springframework/GreetingTest.java
@@ -0,0 +1,16 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GreetingTest {
+
+    @Test
+    void helloWorld() {
+    }
+
+    @Test
+    void helloWorld1() {
+    }
+}
