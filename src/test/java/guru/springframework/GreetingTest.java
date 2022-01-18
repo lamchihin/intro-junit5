5d333346bef50d15e816fb7a62d2e5b86dea1a40
plam <smallshell@gmail.com>
2022-01-17 18:09:02 -0800

    Tried junit lifecycle

--- src/test/java/guru/springframework/GreetingTest.java
+++ src/test/java/guru/springframework/GreetingTest.java
@@ -1,16 +1,44 @@
 package guru.springframework;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GreetingTest {
+    Greeting greeting;
+
+    @BeforeEach
+    void setUp() {
+        System.out.println("@BeforeEach");
+        greeting = new Greeting();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("@AfterAll");
+    }
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("@BeforeAll");
+    }
 
     @Test
     void helloWorld() {
+        System.out.println(greeting.helloWorld());
     }
 
     @Test
     void helloWorld1() {
+        System.out.println(greeting.helloWorld("John"));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("AfterEach");
     }
 }
